9263c5c5b5dc576756cd60471c37adaf0117ec83
Eric <notericsoft@gmail.com>
2018-05-13 01:44:29 -0500

    Update gitignore, launch.bat, removed build/classes

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 @echo off
 @title Development v117.2
-set CLASSPATH=.;dist\MapleAscension.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar;dist\bcprov-jdk16-145.jar
+set CLASSPATH=.;dist\DEV.jar;dist\mina-core.jar;dist\slf4j-api.jar;dist\slf4j-jdk14.jar;dist\mysql-connector-java-bin.jar;dist\bcprov-jdk16-145.jar
 java -server -Dnet.sf.odinms.wzpath=wz server.Start
 pause
